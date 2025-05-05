091bd8fe184076c0398ca0c49aa92c441f9e8e36
Anıl Karacan <aanilkaracann@gmail.com>
2025-05-05 13:23:08 +0300

    Firebase is available. FirebaseAuth is added. Login and Register functions are working.

--- SocialMediaApp/Controllers/LoginViewController.swift
+++ SocialMediaApp/Controllers/LoginViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var alertLabel: UILabel!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        alertLabel.isHidden = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+            view.addGestureRecognizer(tapGesture)
     }
-
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error{
+                    print(e.localizedDescription)
+                    self.alertLabel.isHidden = false
+                    self.alertLabel.text = e.localizedDescription
+                }else{
+                    print("Kullanıcı girişi başarılı")
+                }
+            }
+        }
+    }
+    
+    
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error{
+                    print(e.localizedDescription)
+                    self.alertLabel.isHidden = false
+                    self.alertLabel.text = e.localizedDescription
+                }else{
+                    print("Kullanıcı kaydı başarılı")
+                }
+            }
+        }
+    }
+    
+    
+    
+    
 
 }
 
